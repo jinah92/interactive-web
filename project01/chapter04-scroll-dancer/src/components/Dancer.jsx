c9ce963ace6d335aaa52725363f1e0cd61be6a55
jinah92 <jinah0818@gmail.com>
2025-09-23 16:47:29 +0900

    feat: glb파일 로드 및 애니메이션 재생

---
+++ project01/chapter04-scroll-dancer/src/components/Dancer.jsx
@@ -0,0 +1,20 @@
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+
+export const Dancer = () => {
+  const dancerRef = useRef(null);
+  const { scene, animations } = useGLTF("/models/dancer.glb");
+
+  const { actions } = useAnimations(animations, dancerRef);
+
+  useEffect(() => {
+    actions["wave"].play();
+  }, []);
+
+  return (
+    <>
+      <ambientLight intensity={2} />
+      <primitive ref={dancerRef} object={scene} scale={0.05} />
+    </>
+  );
+};
