a71e502f3ca0be66dba6c51ec036f1044460739c
jinah92 <jinah0818@gmail.com>
2025-09-23 15:55:59 +0900

    feat: 메인 캔버스 세팅

--- project01/chapter04-scroll-dancer/src/App.jsx
+++ project01/chapter04-scroll-dancer/src/App.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import { MainCanvas } from "./components/MainCanvas";
+
 function App() {
-  return <>스크롤 댄서 프로젝트</>;
+  return (
+    <Wrapper>
+      <MainCanvas />
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
