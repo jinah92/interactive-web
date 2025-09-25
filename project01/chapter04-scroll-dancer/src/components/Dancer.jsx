b75479738b9bcde8b5108815d8b13887399d59f6
jinah92 <jinah0818@gmail.com>
2025-09-25 14:22:26 +0900

    feat: 스크롤 이벤트로 오브젝트 제어

--- project01/chapter04-scroll-dancer/src/components/Dancer.jsx
+++ project01/chapter04-scroll-dancer/src/components/Dancer.jsx
@@ -51,15 +51,43 @@ export const Dancer = () => {
     if (!dancerRef.current) return;
 
     timeline = gsap.timeline();
-    timeline.from(
-      dancerRef.current.rotation,
-      {
-        duration: 4,
-        y: -4 * Math.PI,
-      },
-      0.5
-    );
-  }, [isEntered]);
+    timeline
+      .from(
+        dancerRef.current.rotation,
+        {
+          duration: 4,
+          y: -4 * Math.PI,
+        },
+        0.5
+      )
+      .from(
+        dancerRef.current.position,
+        {
+          duration: 4,
+          x: 3,
+        },
+        "<"
+      )
+      .to(
+        three.camera.position,
+        {
+          duration: 10,
+          x: 2,
+          z: 8,
+        },
+        "<"
+      )
+      .to(three.camera.position, {
+        duration: 10,
+        x: 0,
+        z: 6,
+      })
+      .to(three.camera.position, {
+        duration: 10,
+        x: 0,
+        z: 16,
+      });
+  }, [isEntered, three.camera.position]);
 
   if (isEntered) {
     return (
