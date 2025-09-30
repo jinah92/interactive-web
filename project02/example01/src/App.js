c03ec00850240a03400973f69100d4b1ccc24e10
jinah92 <jinah0818@gmail.com>
2025-09-30 14:30:15 +0900

    feat: 3D object 로드방법 실습

--- project02/example01/src/App.js
+++ project02/example01/src/App.js
@@ -1,31 +1,27 @@
-import { useHelper } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useRef } from "react";
-import * as THREE from "three";
 
-function Lights() {
-  const ref = useRef();
-  useHelper(ref, THREE.DirectionalLightHelper, 1, "red");
+function Model(props) {
+  const glb = useGLTF("/models/earth.glb");
+  console.log(glb);
 
-  return <directionalLight ref={ref} position={[1, 3, -1]} intensity={3} />;
-}
-
-function Box(props) {
   return (
     <mesh {...props}>
-      <boxGeometry args={[1]} />
-      <meshStandardMaterial color={"hotpink"} wireframe />
+      <primitive object={glb.scene} />
     </mesh>
   );
 }
 
+function Lights() {
+  return <directionalLight position={[1, 3, -1]} intensity={3} />;
+}
+
 function App() {
   return (
-    <Canvas camera={{ position: [0, 1, 2] }}>
+    <Canvas camera={{ position: [0, 1, 5] }}>
       <color attach="background" args={[100, 200, 100]}></color>
-      <Box rotation-y={1} />
-      <Box position={[0, 0, -1.5]} rotation-y={1} />
       <Lights />
+      <Model position={[0, -2, 0]} />
     </Canvas>
   );
 }
